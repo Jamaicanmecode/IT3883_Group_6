22cc810c8f5b172c4e239224cc93b79df2d7b081
Devonte Pennamon <devonte.pennamon@gmail.com>
2017-11-11 17:30:41 -0500

    gui changes

--- Vandeven_Shuttle/Form1.Designer.cs
+++ Vandeven_Shuttle/Form1.Designer.cs
@@ -28,12 +28,324 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.reservationDetailsGroupBox = new System.Windows.Forms.GroupBox();
+            this.passengersComboBox = new System.Windows.Forms.ComboBox();
+            this.passengersLabel = new System.Windows.Forms.Label();
+            this.destinationComboBox = new System.Windows.Forms.ComboBox();
+            this.returningDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.returningLabel = new System.Windows.Forms.Label();
+            this.departingDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.methodOfReservationComboBox = new System.Windows.Forms.ComboBox();
+            this.departingLabel = new System.Windows.Forms.Label();
+            this.destinationLabel = new System.Windows.Forms.Label();
+            this.methodOfReservationLabel = new System.Windows.Forms.Label();
+            this.customerInformationGroupBox = new System.Windows.Forms.GroupBox();
+            this.creditCardLabel = new System.Windows.Forms.Label();
+            this.phoneNumberLabel = new System.Windows.Forms.Label();
+            this.homeAddressLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.creditCardNumberTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.homeAddressTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.existingUserButton = new System.Windows.Forms.Button();
+            this.newUserButton = new System.Windows.Forms.Button();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.reservationDetailsGroupBox.SuspendLayout();
+            this.customerInformationGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // reservationDetailsGroupBox
+            // 
+            this.reservationDetailsGroupBox.Controls.Add(this.passengersComboBox);
+            this.reservationDetailsGroupBox.Controls.Add(this.passengersLabel);
+            this.reservationDetailsGroupBox.Controls.Add(this.destinationComboBox);
+            this.reservationDetailsGroupBox.Controls.Add(this.returningDateTimePicker);
+            this.reservationDetailsGroupBox.Controls.Add(this.returningLabel);
+            this.reservationDetailsGroupBox.Controls.Add(this.departingDateTimePicker);
+            this.reservationDetailsGroupBox.Controls.Add(this.methodOfReservationComboBox);
+            this.reservationDetailsGroupBox.Controls.Add(this.departingLabel);
+            this.reservationDetailsGroupBox.Controls.Add(this.destinationLabel);
+            this.reservationDetailsGroupBox.Controls.Add(this.methodOfReservationLabel);
+            this.reservationDetailsGroupBox.Location = new System.Drawing.Point(315, 22);
+            this.reservationDetailsGroupBox.Name = "reservationDetailsGroupBox";
+            this.reservationDetailsGroupBox.Size = new System.Drawing.Size(330, 290);
+            this.reservationDetailsGroupBox.TabIndex = 7;
+            this.reservationDetailsGroupBox.TabStop = false;
+            this.reservationDetailsGroupBox.Text = "Reservation Details";
+            // 
+            // passengersComboBox
+            // 
+            this.passengersComboBox.FormattingEnabled = true;
+            this.passengersComboBox.Location = new System.Drawing.Point(35, 237);
+            this.passengersComboBox.Name = "passengersComboBox";
+            this.passengersComboBox.Size = new System.Drawing.Size(121, 21);
+            this.passengersComboBox.TabIndex = 13;
+            // 
+            // passengersLabel
+            // 
+            this.passengersLabel.AutoSize = true;
+            this.passengersLabel.Location = new System.Drawing.Point(35, 209);
+            this.passengersLabel.Name = "passengersLabel";
+            this.passengersLabel.Size = new System.Drawing.Size(65, 13);
+            this.passengersLabel.TabIndex = 12;
+            this.passengersLabel.Text = "Passengers:";
+            // 
+            // destinationComboBox
+            // 
+            this.destinationComboBox.FormattingEnabled = true;
+            this.destinationComboBox.Location = new System.Drawing.Point(190, 86);
+            this.destinationComboBox.Name = "destinationComboBox";
+            this.destinationComboBox.Size = new System.Drawing.Size(121, 21);
+            this.destinationComboBox.TabIndex = 11;
+            // 
+            // returningDateTimePicker
+            // 
+            this.returningDateTimePicker.CustomFormat = "MM/dd/yy";
+            this.returningDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.returningDateTimePicker.Location = new System.Drawing.Point(181, 161);
+            this.returningDateTimePicker.Name = "returningDateTimePicker";
+            this.returningDateTimePicker.Size = new System.Drawing.Size(76, 20);
+            this.returningDateTimePicker.TabIndex = 10;
+            // 
+            // returningLabel
+            // 
+            this.returningLabel.AutoSize = true;
+            this.returningLabel.Location = new System.Drawing.Point(178, 132);
+            this.returningLabel.Name = "returningLabel";
+            this.returningLabel.Size = new System.Drawing.Size(56, 13);
+            this.returningLabel.TabIndex = 9;
+            this.returningLabel.Text = "Returning:";
+            // 
+            // departingDateTimePicker
+            // 
+            this.departingDateTimePicker.CustomFormat = "MM/dd/yy";
+            this.departingDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.departingDateTimePicker.Location = new System.Drawing.Point(35, 161);
+            this.departingDateTimePicker.Name = "departingDateTimePicker";
+            this.departingDateTimePicker.Size = new System.Drawing.Size(76, 20);
+            this.departingDateTimePicker.TabIndex = 8;
+            // 
+            // methodOfReservationComboBox
+            // 
+            this.methodOfReservationComboBox.FormattingEnabled = true;
+            this.methodOfReservationComboBox.Location = new System.Drawing.Point(190, 39);
+            this.methodOfReservationComboBox.Name = "methodOfReservationComboBox";
+            this.methodOfReservationComboBox.Size = new System.Drawing.Size(121, 21);
+            this.methodOfReservationComboBox.TabIndex = 7;
+            // 
+            // departingLabel
+            // 
+            this.departingLabel.AutoSize = true;
+            this.departingLabel.Location = new System.Drawing.Point(35, 132);
+            this.departingLabel.Name = "departingLabel";
+            this.departingLabel.Size = new System.Drawing.Size(56, 13);
+            this.departingLabel.TabIndex = 6;
+            this.departingLabel.Text = "Departing:";
+            // 
+            // destinationLabel
+            // 
+            this.destinationLabel.AutoSize = true;
+            this.destinationLabel.Location = new System.Drawing.Point(35, 86);
+            this.destinationLabel.Name = "destinationLabel";
+            this.destinationLabel.Size = new System.Drawing.Size(63, 13);
+            this.destinationLabel.TabIndex = 5;
+            this.destinationLabel.Text = "Destination:";
+            // 
+            // methodOfReservationLabel
+            // 
+            this.methodOfReservationLabel.AutoSize = true;
+            this.methodOfReservationLabel.Location = new System.Drawing.Point(32, 42);
+            this.methodOfReservationLabel.Name = "methodOfReservationLabel";
+            this.methodOfReservationLabel.Size = new System.Drawing.Size(118, 13);
+            this.methodOfReservationLabel.TabIndex = 4;
+            this.methodOfReservationLabel.Text = "Method of Reservation:";
+            // 
+            // customerInformationGroupBox
+            // 
+            this.customerInformationGroupBox.Controls.Add(this.emailTextBox);
+            this.customerInformationGroupBox.Controls.Add(this.emailLabel);
+            this.customerInformationGroupBox.Controls.Add(this.creditCardLabel);
+            this.customerInformationGroupBox.Controls.Add(this.phoneNumberLabel);
+            this.customerInformationGroupBox.Controls.Add(this.homeAddressLabel);
+            this.customerInformationGroupBox.Controls.Add(this.lastNameLabel);
+            this.customerInformationGroupBox.Controls.Add(this.firstNameLabel);
+            this.customerInformationGroupBox.Controls.Add(this.creditCardNumberTextBox);
+            this.customerInformationGroupBox.Controls.Add(this.phoneNumberTextBox);
+            this.customerInformationGroupBox.Controls.Add(this.homeAddressTextBox);
+            this.customerInformationGroupBox.Controls.Add(this.lastNameTextBox);
+            this.customerInformationGroupBox.Controls.Add(this.firstNameTextBox);
+            this.customerInformationGroupBox.Location = new System.Drawing.Point(14, 22);
+            this.customerInformationGroupBox.Name = "customerInformationGroupBox";
+            this.customerInformationGroupBox.Size = new System.Drawing.Size(250, 329);
+            this.customerInformationGroupBox.TabIndex = 6;
+            this.customerInformationGroupBox.TabStop = false;
+            this.customerInformationGroupBox.Text = "Customer Information";
+            // 
+            // creditCardLabel
+            // 
+            this.creditCardLabel.AutoSize = true;
+            this.creditCardLabel.Location = new System.Drawing.Point(8, 237);
+            this.creditCardLabel.Name = "creditCardLabel";
+            this.creditCardLabel.Size = new System.Drawing.Size(102, 13);
+            this.creditCardLabel.TabIndex = 9;
+            this.creditCardLabel.Text = "Credit Card Number:";
+            // 
+            // phoneNumberLabel
+            // 
+            this.phoneNumberLabel.AutoSize = true;
+            this.phoneNumberLabel.Location = new System.Drawing.Point(8, 188);
+            this.phoneNumberLabel.Name = "phoneNumberLabel";
+            this.phoneNumberLabel.Size = new System.Drawing.Size(81, 13);
+            this.phoneNumberLabel.TabIndex = 8;
+            this.phoneNumberLabel.Text = "Phone Number:";
+            // 
+            // homeAddressLabel
+            // 
+            this.homeAddressLabel.AutoSize = true;
+            this.homeAddressLabel.Location = new System.Drawing.Point(8, 139);
+            this.homeAddressLabel.Name = "homeAddressLabel";
+            this.homeAddressLabel.Size = new System.Drawing.Size(79, 13);
+            this.homeAddressLabel.TabIndex = 7;
+            this.homeAddressLabel.Text = "Home Address:";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(8, 89);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(61, 13);
+            this.lastNameLabel.TabIndex = 6;
+            this.lastNameLabel.Text = "Last Name:";
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(8, 36);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(60, 13);
+            this.firstNameLabel.TabIndex = 5;
+            this.firstNameLabel.Text = "First Name:";
+            // 
+            // creditCardNumberTextBox
+            // 
+            this.creditCardNumberTextBox.Location = new System.Drawing.Point(117, 234);
+            this.creditCardNumberTextBox.Name = "creditCardNumberTextBox";
+            this.creditCardNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.creditCardNumberTextBox.TabIndex = 4;
+            // 
+            // phoneNumberTextBox
+            // 
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(117, 185);
+            this.phoneNumberTextBox.Name = "phoneNumberTextBox";
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.phoneNumberTextBox.TabIndex = 3;
+            // 
+            // homeAddressTextBox
+            // 
+            this.homeAddressTextBox.Location = new System.Drawing.Point(117, 136);
+            this.homeAddressTextBox.Name = "homeAddressTextBox";
+            this.homeAddressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.homeAddressTextBox.TabIndex = 2;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(117, 86);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lastNameTextBox.TabIndex = 1;
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(117, 33);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.firstNameTextBox.TabIndex = 0;
+            // 
+            // existingUserButton
+            // 
+            this.existingUserButton.Location = new System.Drawing.Point(387, 373);
+            this.existingUserButton.Name = "existingUserButton";
+            this.existingUserButton.Size = new System.Drawing.Size(78, 23);
+            this.existingUserButton.TabIndex = 5;
+            this.existingUserButton.Text = "Existing User";
+            this.existingUserButton.UseVisualStyleBackColor = true;
+            // 
+            // newUserButton
+            // 
+            this.newUserButton.Location = new System.Drawing.Point(186, 373);
+            this.newUserButton.Name = "newUserButton";
+            this.newUserButton.Size = new System.Drawing.Size(78, 23);
+            this.newUserButton.TabIndex = 4;
+            this.newUserButton.Text = "New User";
+            this.newUserButton.UseVisualStyleBackColor = true;
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(8, 277);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(79, 13);
+            this.emailLabel.TabIndex = 10;
+            this.emailLabel.Text = "E-mail Address:";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(117, 274);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(100, 20);
+            this.emailTextBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 435);
+            this.Controls.Add(this.reservationDetailsGroupBox);
+            this.Controls.Add(this.customerInformationGroupBox);
+            this.Controls.Add(this.existingUserButton);
+            this.Controls.Add(this.newUserButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.reservationDetailsGroupBox.ResumeLayout(false);
+            this.reservationDetailsGroupBox.PerformLayout();
+            this.customerInformationGroupBox.ResumeLayout(false);
+            this.customerInformationGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox reservationDetailsGroupBox;
+        private System.Windows.Forms.ComboBox passengersComboBox;
+        private System.Windows.Forms.Label passengersLabel;
+        private System.Windows.Forms.ComboBox destinationComboBox;
+        private System.Windows.Forms.DateTimePicker returningDateTimePicker;
+        private System.Windows.Forms.Label returningLabel;
+        private System.Windows.Forms.DateTimePicker departingDateTimePicker;
+        private System.Windows.Forms.ComboBox methodOfReservationComboBox;
+        private System.Windows.Forms.Label departingLabel;
+        private System.Windows.Forms.Label destinationLabel;
+        private System.Windows.Forms.Label methodOfReservationLabel;
+        private System.Windows.Forms.GroupBox customerInformationGroupBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label creditCardLabel;
+        private System.Windows.Forms.Label phoneNumberLabel;
+        private System.Windows.Forms.Label homeAddressLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox creditCardNumberTextBox;
+        private System.Windows.Forms.TextBox phoneNumberTextBox;
+        private System.Windows.Forms.TextBox homeAddressTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Button existingUserButton;
+        private System.Windows.Forms.Button newUserButton;
     }
 }
 
